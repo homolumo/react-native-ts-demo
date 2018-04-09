98f172fcda846c9f126d0c06ef26386012849d41
chenjingyuan <chenjingyuan@ejiaoji.com>
2018-04-09 14:01:42 +0800

    1. update to rn 0.55.1 2. add antd-mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button, Popover, Toast } from 'antd-mobile';
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, View } from 'react-native';
 
@@ -6,22 +7,43 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
 });
 
-export default class App extends React.Component<{}> {
+interface IProps {
+  test: string;
+}
+
+interface IState {
+  result: string;
+}
+
+export default class App extends React.Component<IProps, IState> {
+  // public static getDerivedStateFromProps(
+  //   nextProps: Readonly<IProps>,
+  //   prevState: IState,
+  // ): Partial<IState> | null {
+
+  // }
+
+  public componentDidMount() {
+
+    fetch('https://test.ejiaoji.com/appjiaoji/api/index/list').then((r) => {
+      r.json().then((json) => {
+        console.log(json);
+        Toast.info('ok', 1.5);
+      });
+    });
+  }
+
   public render() {
     const pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
     };
-    fetch('https://test.ejiaoji.com/appjiaoji/api/index/list').then((r) => {
-      r.json().then((json) => {
-        console.log(json);
-      });
-    });
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
         <Image source={pic} style={{ width: 193, height: 110 }} />
+        <Button type="primary">Click me!</Button>
       </View>
     );
   }
